773f3338e5385821f248d17f8ffb5c21b2f2556d
Erkan Kamber <63397975+erkan9@users.noreply.github.com>
2020-04-18 18:41:31 +0300

    Homework_W2_Ex3
    Homework
    Week 2 
    Exercise 3

--- Homework_W2_3.java
+++ Homework_W2_3.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Homework_W2_3 {
 
     public static void main(String[] args) {
+        
         Scanner scanner = new Scanner(System.in);
         
         int floors = 9;

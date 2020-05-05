30c267cea280145ee5432819aca3d8d5d31541f5
Erkan Kamber <63397975+erkan9@users.noreply.github.com>
2020-05-05 11:34:55 +0300

    TheKingIsDead
    The King is dead
    Week 4
    Exercise 1 of 1

--- chessForPonies.java
+++ chessForPonies.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class chessForPonies {
+public class ChessForPonies {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);

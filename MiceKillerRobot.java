438a7e55cf667f1e2f298fe6b2b56255ab953c9e
Erkan Kamber <63397975+erkan9@users.noreply.github.com>
2020-05-06 16:42:11 +0300

    Update MiceKillerRobot.java

--- MiceKillerRobot.java
+++ MiceKillerRobot.java
@@ -1,7 +1,7 @@
 import java.util.Random;
 import java.util.Scanner;
 
-public class Mice_Killer_Robot {
+public class MiceKillerRobot {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
